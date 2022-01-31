88e877155855e6b1f599693bafa9f41c4cec4cde
Vladimir Netkachev <vovanok.net@gmail.com>
2022-01-31 13:14:14 +0300

    Initial Commit

---
+++ SweetHomeMusic/SweetHomeMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweetHomeMusicApp.swift
+//  SweetHomeMusic
+//
+//  Created by Vladimir Netkachev on 31.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SweetHomeMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
